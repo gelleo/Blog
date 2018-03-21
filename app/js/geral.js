e9cf4ced600b4819583c8e89b5b566692f1befd9
gelleo <gelleo@gmail.com>
2018-03-21 12:59:12 -0300

    Add files via upload

---
+++ app/js/geral.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.data-table').DataTable();
+    tinymce.init({
+    	  selector: 'textarea',
+    	  height: 500,
+    	  theme: 'modern',
+    	  plugins: 'print preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help',
+    	  toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+    	  image_advtab: true,
+    	  templates: [
+    	    { title: 'Test template 1', content: 'Test 1' },
+    	    { title: 'Test template 2', content: 'Test 2' }
+    	  ]
+    	 });
+} );
